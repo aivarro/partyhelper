378210d89b6e9e319a5e4d133977210474b1bb52
aivarro <aivar.romandi@gmail.com>
2026-06-01 13:33:03 +0300

    removed CoT from api response

--- src/main/java/party/hard/partyhelper/model/PartyDecision.java
+++ src/main/java/party/hard/partyhelper/model/PartyDecision.java
@@ -1,6 +1,9 @@
 package party.hard.partyhelper.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public record PartyDecision(boolean shouldGoOut,
+                            @JsonIgnore
                             String chainOfThought,
                             String recommendedParty,
                             String markdownMessage) {
