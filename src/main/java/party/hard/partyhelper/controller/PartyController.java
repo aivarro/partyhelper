e7985feacc6c6ca08aa0d201c2ee49aaa0d311c3
aivarro <aivar.romandi@gmail.com>
2026-06-01 13:08:41 +0300

    removed comments

--- src/main/java/party/hard/partyhelper/controller/PartyController.java
+++ src/main/java/party/hard/partyhelper/controller/PartyController.java
@@ -12,14 +12,12 @@ public class PartyController {
 
     private final PartyService partyService;
 
-    // Süstime teenuse kontrollerisse
     public PartyController(PartyService partyService) {
         this.partyService = partyService;
     }
 
     @PostMapping("/decide")
     public PartyDecision decideParty(@Valid @RequestBody UserInput input) {
-        // Kontroller teeb ainult ühe asja: küsib teenuselt vastust!
         return partyService.getPartyRecommendation(input);
     }
 }
